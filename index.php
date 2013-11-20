f25277e0c2bfe8a9eafad981473961fa719658d8
atsushi watanabe <watanabe@sunrisedigital.jp>
2013-11-20 15:56:09 +0900

    試行錯誤中。

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>improve-condition</title>
-	<link rel="stylesheet" href="/css/common.css" type="text/css" media="all">
+	<link rel="stylesheet" href="/improve-condition/css/index.css" type="text/css" media="all">
 </head>
 <body>
 
